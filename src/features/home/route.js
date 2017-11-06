ac176c385465fa496b9c4f4ad7e29812935f424e
Nate Wang <nate.wang@sap.com>
2017-11-06 11:47:20 +0800

    Initial import.

---
+++ src/features/home/route.js
@@ -0,0 +1,17 @@
+import {
+  DefaultPage,
+  TestPage,
+} from './';
+
+export default {
+  path: '/',
+  name: 'Home',
+  childRoutes: [
+    { path: 'default-page',
+      name: 'Default page',
+      component: DefaultPage,
+      isIndex: true,
+    },
+    { path: 'test-page', name: 'Test page', component: TestPage },
+  ],
+};
